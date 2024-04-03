a5d9a5184cbbc6231acae5b5059ff6e28d2b657b
Johnblesson <johnblessonrowe@gmail.com>
2024-04-03 09:12:47 +0000

    update

--- server/routes/contactRoute.js
+++ server/routes/contactRoute.js
@@ -11,6 +11,7 @@ import {
   
 import { ensureAuthenticated } from "../middleware/isAuth.js"; 
 import { isAdmin } from "../middleware/isAdmin.js";
+import { checkSudoMiddleware } from "../../middleware/checkSudoMiddleware.js";
 // import { checkIpAccess } from "../middleware/checkip.js";   
 // import { superAdminOnly } from "../middleware/sudo.js";
 
@@ -19,7 +20,7 @@ router.get('/message', ensureAuthenticated, isAdmin, messagesRoute);
 router.get('/get-all-contact', ensureAuthenticated, isAdmin, getAllContactForms);
 router.get('/view-message/:id', ensureAuthenticated, isAdmin, messageView)
 
-router.delete('/delete-message/:id',  ensureAuthenticated, isAdmin, deleteMessage);
+router.delete('/delete-message/:id',  ensureAuthenticated, isAdmin, checkSudoMiddleware, deleteMessage);
 router.get('/delete-message/:id',  ensureAuthenticated, isAdmin, deleteMessage);
 
 export default router;
