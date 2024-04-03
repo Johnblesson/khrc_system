1352a34d58931ac2698aeb2e3978c31b63ed0986
Johnblesson <johnblessonrowe@gmail.com>
2024-04-03 10:16:42 +0000

    update

--- server/middleware/checkip.js
+++ server/middleware/checkip.js
@@ -1,3 +1,19 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// export const checkIpAccess = (req, res, next) => {
+//     // Define an array of allowed IP addresses
+//     const allowedIps = process.env.ALLOWED_IPS.split(',');
+
+//     // Get the client's IP address from the request object
+//     const clientIp = req.ip;
+//     if (allowedIps.includes(clientIp)) {
+//         next();
+//     } else {
+//         res.redirect('/forbidden');
+//     }
+//     }
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,10 +22,12 @@ export const checkIpAccess = (req, res, next) => {
     const allowedIps = process.env.ALLOWED_IPS.split(',');
 
     // Get the client's IP address from the request object
-    const clientIp = req.ip;
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
     if (allowedIps.includes(clientIp)) {
         next();
     } else {
-        res.redirect('/forbidden');
+        res.status(403).send('Forbidden');
     }
-    }
+};
+
