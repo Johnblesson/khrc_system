7d8ce43032335c4547b3b51fe6b251e117f2b4fe
Johnblesson <johnblessonrowe@gmail.com>
2024-04-03 10:45:30 +0000

    update

--- server.js
+++ server.js
@@ -53,13 +53,13 @@ app.use(methodOverride('_method'));
 // app.disable('x-powered-by');
 
 // Disable ETag header
-app.set('etag', false);
+// app.set('etag', false);
 
 // Disable Server signature
-app.use((req, res, next) => {
-  res.removeHeader('Server');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.removeHeader('Server');
+//   next();
+// });
 
 // Add the router
 app.use(views)
