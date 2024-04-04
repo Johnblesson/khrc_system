7a86f19861aaef14fc790dd1bfa9837c30a31294
Johnblesson <johnblessonrowe@gmail.com>
2024-04-04 08:13:59 +0000

    fixed middleware

--- server/middleware/sudo.js
+++ server/middleware/sudo.js
@@ -1,3 +1,19 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// export const checkForIpAddress = (req, res, next) => {
+//     // Define an array of allowed IP addresses
+//     const allowedIps = process.env.SUPER_ADMIN_IPS.split(',');
+
+//     // Get the client's IP address from the request object
+//     const clientIp = req.ip;
+//     if (allowedIps.includes(clientIp)) {
+//         next();
+//     } else {
+//         res.redirect('/forbidden');
+//     }
+//     };
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -5,11 +21,18 @@ export const checkForIpAddress = (req, res, next) => {
     // Define an array of allowed IP addresses
     const allowedIps = process.env.SUPER_ADMIN_IPS.split(',');
 
-    // Get the client's IP address from the request object
-    const clientIp = req.ip;
-    if (allowedIps.includes(clientIp)) {
+    // Get the client's IP address from the request headers, considering proxy
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    // Split x-forwarded-for to handle multiple proxy servers
+    const forwardedIps = clientIp ? clientIp.split(',') : [];
+
+    // Extract the first IP address from the forwarded headers
+    const firstIp = forwardedIps.length > 0 ? forwardedIps[0].trim() : null;
+
+    if (allowedIps.includes(firstIp)) {
         next();
     } else {
         res.redirect('/forbidden');
     }
-    };
+};
