c6b58098d4288fc11402de0b2557461f0460fb9e
Johnblesson <johnblessonrowe@gmail.com>
2024-04-04 08:54:59 +0000

    update route

--- server/middleware/isAuth.js
+++ server/middleware/isAuth.js
@@ -1,12 +1,4 @@
 // Middleware to check if a user is logged in
-// export const isAuthenticated = (req, res, next) => {
-//     if (req.session.user) {
-//       next();
-//     } else {
-//       res.status(401).json({ error: 'Unauthorized' });
-//     }
-//   }
-
 export const ensureAuthenticated = (req, res, next) => {
   if (req.session.user) {
       // User is authenticated, allow them to proceed
