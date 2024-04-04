b636138372e0c74eb63fa79da991c2289fb9e531
Johnblesson <johnblessonrowe@gmail.com>
2024-04-04 07:45:43 +0000

    update

--- server/middleware/checkip.js
+++ server/middleware/checkip.js
@@ -17,22 +17,43 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+// export const checkIpAccess = (req, res, next) => {
+//     // Define an array of allowed IP addresses
+//     const allowedIps = process.env.ALLOWED_IPS.split(',');
+//     console.log(process.env.ALLOWED_IPS)
+
+//     // Get the client's IP address from the request headers, considering proxy
+//     const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+//     // Split x-forwarded-for to handle multiple proxy servers
+//     const forwardedIps = clientIp ? clientIp.split(',') : [];
+
+//     // Extract the first IP address from the forwarded headers
+//     const firstIp = forwardedIps.length > 0 ? forwardedIps[0].trim() : null;
+
+//     if (firstIp && allowedIps.includes(firstIp)) {
+//         next();
+//     } else {
+//         res.redirect('/forbidden');
+//     }
+// };
+
 export const checkIpAccess = (req, res, next) => {
-    // Define an array of allowed IP addresses
     const allowedIps = process.env.ALLOWED_IPS.split(',');
+    
+    // Get the client's IP address from the request headers
+    let clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-    // Get the client's IP address from the request headers, considering proxy
-    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    // Handle multiple proxy servers by taking the first IP address
+    if (clientIp) {
+        const forwardedIps = clientIp.split(',');
+        clientIp = forwardedIps[0].trim();
+    }
 
-    // Split x-forwarded-for to handle multiple proxy servers
-    const forwardedIps = clientIp ? clientIp.split(',') : [];
-
-    // Extract the first IP address from the forwarded headers
-    const firstIp = forwardedIps.length > 0 ? forwardedIps[0].trim() : null;
-
-    if (firstIp && allowedIps.includes(firstIp)) {
+    if (allowedIps.includes(clientIp)) {
         next();
     } else {
-        res.redirect('/forbidden');
+        res.redirect('/Forbidden');
     }
 };
+
