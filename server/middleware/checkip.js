ec94ebddb232d527e190c7d16f4850f7cd3e0419
Johnblesson <johnblessonrowe@gmail.com>
2024-04-04 07:37:07 +0000

    update

--- server/middleware/checkip.js
+++ server/middleware/checkip.js
@@ -14,6 +14,7 @@
 //     }
 //     }
 
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -21,35 +22,18 @@ export const checkIpAccess = (req, res, next) => {
     // Define an array of allowed IP addresses
     const allowedIps = process.env.ALLOWED_IPS.split(',');
 
-    // Get the client's IP address from the request object
+    // Get the client's IP address from the request headers, considering proxy
     const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-    if (allowedIps.includes(clientIp)) {
+    // Split x-forwarded-for to handle multiple proxy servers
+    const forwardedIps = clientIp ? clientIp.split(',') : [];
+
+    // Extract the first IP address from the forwarded headers
+    const firstIp = forwardedIps.length > 0 ? forwardedIps[0].trim() : null;
+
+    if (firstIp && allowedIps.includes(firstIp)) {
         next();
     } else {
         res.redirect('/forbidden');
     }
 };
-
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// export const checkIpAccess = (req, res, next) => {
-//     // Define an array of allowed IP addresses
-//     const allowedIps = process.env.ALLOWED_IPS.split(',');
-
-//     // Get the client's IP address from the request headers, considering proxy
-//     const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-//     // Split x-forwarded-for to handle multiple proxy servers
-//     const forwardedIps = clientIp ? clientIp.split(',') : [];
-
-//     // Extract the first IP address from the forwarded headers
-//     const firstIp = forwardedIps.length > 0 ? forwardedIps[0].trim() : null;
-
-//     if (firstIp && allowedIps.includes(firstIp)) {
-//         next();
-//     } else {
-//         res.redirect('/forbidden');
-//     }
-// };
